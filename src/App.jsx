19d8b51a3dd54dfed98fb2c4fd16fe86583d22b4
TheDarkLady <lakshmichaithanya13@gmail.com>
2024-11-01 22:06:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from 'react-router-dom'
+import './App.css'
+function App() {
+  const router = createBrowserRouter()
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Hello This is lakshmi</h1>
+    </div>
+  )
+}
+
+export default App
