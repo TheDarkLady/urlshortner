0739def310fd42ba9d91ca313c12f5da36e08433
TheDarkLady <lakshmichaithanya13@gmail.com>
2024-11-07 01:07:05 +0530

    accortion and banner image are added

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { Input } from "@/components/ui/input";
 
 function Landing() {
-  return (
-    <div>
-      <h1>Landing page</h1>
+  return <div className="w-full h-full flex flex-col justify-center items-center">
+    <div className="w-full max-width-[1200px] mx-auto flex flex-col justify-center items-center p-5">
+      <h2 className="my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl font-extrabold text-center">The only url shortner you'll ever need</h2>
+      <form>
+        <Input/>
+        <Button>Shorten !</Button>
+      </form>
+      <img src="/banner1.jpg" className="w-full my-11 md:px-11" alt="shorten banner" />
     </div>
-  )
+  </div>;
 }
 
-export default Landing
+export default Landing;
