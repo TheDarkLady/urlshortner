4b9763bf54b4be08d96d3b3b17524096d80b5f63
TheDarkLady <lakshmichaithanya13@gmail.com>
2024-11-06 23:57:26 +0530

    avatar and icons are added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,46 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Button } from "./ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
-
-  
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { LinkIcon, LogOut } from "lucide-react";
 function Header() {
   const navigate = useNavigate();
   const user = true;
   return (
     <nav className="py-4 px-8 flex justify-between items-center ">
       <Link to={"/"}>
-        <img
-          src="/logo.png"
-          className="h-16"
-          alt="shorten logo"
-        />
+        <img src="/logo.png" className="h-16" alt="shorten logo" />
       </Link>
       <div>
         {!user ? (
           <Button onClick={() => navigate("/auth")}>Login</Button>
         ) : (
-            <DropdownMenu>
-            <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>The Dark Lady</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
             <DropdownMenuContent>
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuLabel>The Dark Lady</DropdownMenuLabel>
               <DropdownMenuSeparator />
-              <DropdownMenuItem>Profile</DropdownMenuItem>
-              <DropdownMenuItem>Billing</DropdownMenuItem>
-              <DropdownMenuItem>Team</DropdownMenuItem>
-              <DropdownMenuItem>Subscription</DropdownMenuItem>
+              <DropdownMenuItem>
+                <LinkIcon className="mr-2 h-4 w-4" />
+                My Links</DropdownMenuItem>
+              <DropdownMenuItem className="text-red-600">
+                <LogOut className="mr-2 h-4 w-4" />
+                Log Out</DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
-          
         )}
       </div>
     </nav>
