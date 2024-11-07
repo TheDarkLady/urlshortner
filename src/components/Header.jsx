4093119777736fb035b61a622cfa94a2ca940397
TheDarkLady <lakshmichaithanya13@gmail.com>
2024-11-07 13:36:16 +0530

    merged

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Button } from "./ui/button";
+
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -11,23 +13,20 @@ import {
 } from "./ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { LinkIcon, LogOut } from "lucide-react";
+>>>>>>> 0739def310fd42ba9d91ca313c12f5da36e08433
 function Header() {
   const navigate = useNavigate();
   const user = true;
   return (
-    <nav className="py-4 px-8 flex justify-between items-center ">
+    <nav className="py-4 px-24 flex justify-between items-center ">
       <Link to={"/"}>
-        <img src="/logo.png" className="h-16" alt="shorten logo" />
       </Link>
       <div>
         {!user ? (
           <Button onClick={() => navigate("/auth")}>Login</Button>
         ) : (
           <DropdownMenu>
-            <DropdownMenuTrigger>
-              <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>The Dark Lady</AvatarFallback>
+
               </Avatar>
             </DropdownMenuTrigger>
             <DropdownMenuContent>
@@ -36,11 +35,19 @@ function Header() {
               <DropdownMenuItem>
                 <LinkIcon className="mr-2 h-4 w-4" />
                 My Links</DropdownMenuItem>
+<<<<<<< HEAD
+              <DropdownMenuItem className="text-red-400">
+=======
               <DropdownMenuItem className="text-red-600">
+>>>>>>> 0739def310fd42ba9d91ca313c12f5da36e08433
                 <LogOut className="mr-2 h-4 w-4" />
                 Log Out</DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
+<<<<<<< HEAD
+
+=======
+>>>>>>> 0739def310fd42ba9d91ca313c12f5da36e08433
         )}
       </div>
     </nav>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,18 +1,13 @@
-import { Button } from "@/components/ui/button";
-import React from "react";
-import { Input } from "@/components/ui/input";
+import React from 'react'
 
 function Landing() {
-  return <div className="w-full h-full flex flex-col justify-center items-center">
-    <div className="w-full max-width-[1200px] mx-auto flex flex-col justify-center items-center p-5">
-      <h2 className="my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl font-extrabold text-center">The only url shortner you'll ever need</h2>
-      <form>
-        <Input/>
-        <Button>Shorten !</Button>
-      </form>
-      <img src="/banner1.jpg" className="w-full my-11 md:px-11" alt="shorten banner" />
+  return (
+    <div className='w-full h-full'>
+      <div className='w-[1200px] h-full m-auto flex justify-center '>
+
+      </div>
     </div>
-  </div>;
+  )
 }
 
-export default Landing;
+export default Landing
