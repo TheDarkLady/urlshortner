1130f5cea6f170b88566e6bcb38bbe57cec5af83
TheDarkLady <lakshmichaithanya13@gmail.com>
2024-11-12 19:17:40 +0530

    supabase url

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
-  
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { BeatLoader } from "react-spinners";
+import Error from "./Error";
+import * as Yup from 'yup';
+import useFetch from "@/hooks/useFetch";
+import { login } from "@/db/apiauth";
 function Login() {
+  const [errors, setErrors] = useState([])
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  })
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+
+    }))
+  }
+  const { data, error, loading, fn: fnLogin } = useFetch(login, formData);
+  useEffect(()=>{
+    console.log(data);
+  }, [data, error]);
+  const handleLogin = async () => {
+    setErrors([]);
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string().email("Invalid Email").required("Email is required"),
+        password: Yup.string().min(6, "Password must be atleast 6 characters").required("Password is required")
+      });
+      await schema.validate(formData, { abortEarly: false });
+      await fnLogin()
+      // api call
+    }
+    catch (e) {
+      const newErrors = {};
+      e?.inner?.forEach((err) => {
+        newErrors[err.path] = err.message;
+      });
+      setErrors(newErrors)
+    }
+  }
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Card Title</CardTitle>
-        <CardDescription>Card Description</CardDescription>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>Login to your account if you already have one</CardDescription>
+        <Error message="Invalid email" />
       </CardHeader>
-      <CardContent>
-        <p>Card Content</p>
+      <CardContent className="space-y-2">
+        <div className="space-y-1">
+          <Input name="email" placeholder="Enter Email" onChange={handleInputChange} />
+          {errors.email && <Error message={errors.email} />}
+        </div>
+        <div className="space-y-1">
+          <Input name="password" placeholder="Enter password" onChange={handleInputChange} />
+          {errors.email && <Error message={errors.email} />}
+        </div>
       </CardContent>
       <CardFooter>
-        <p>Card Footer</p>
+        <Button onClick={handleLogin}>
+          {loading ? <BeatLoader size={10} color="#36d7b7" /> : "Login"}
+        </Button>
       </CardFooter>
     </Card>
   );
 }
+
 
 export default Login;
