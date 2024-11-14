fbf54998db632ca982d443cdb5219c90c1c83423
TheDarkLady <lakshmichaithanya13@gmail.com>
2024-11-14 15:51:56 +0530

    auth redirected to dashboard

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,16 +1,26 @@
-import React from "react";
-import { useSearchParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import Login from "@/components/Login";
 import Signup from "@/components/Signup";
+import { UrlState } from "@/Context";
 
 
 function Auth() {
   const [searchParams] = useSearchParams();
+  const longLink = searchParams.get("createNew");
+  const navigate = useNavigate();
+
+  const {isAuthenticated, loading} = UrlState();
+  useEffect(()=>{
+    if(isAuthenticated && !loading){
+      navigate(`/dashboard?${longLink ? `createNew=${longLink}` : ""}`)
+    }
+  }, [isAuthenticated, loading])
   return (
     <div className="mt-36 flex flex-col items-center gap-10">
       <h1 className="text-3xl font-extrabold">
-        {searchParams.get("createNew") ? "Hold Up.....Login in first" : ""}
+        {longLink ? "Hold Up.....Login in first" : "Login / Signup"}
       </h1>
       <Tabs defaultValue="login" className="w-[400px]">
         <TabsList className="w-full">
